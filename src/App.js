a9ef40206c7c224507187f11f5bb7677a39079bf
Jeremias Rincaveski <jrincaveski@gmail.com>
2024-06-21 09:13:27 -0300

    iniciando projeto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     0000 0000 0000 0000
+    Jane Appleseed
+    00/00
+
+    000
+
+    Cardholder Name
+    e.g. Jane Appleseed
+
+    Card Number
+    e.g. 1234 5678 9123 0000
+
+    Exp. Date (MM/YY)
+    MM
+    YY
+
+    CVC
+    e.g. 123
+
+    Confirm
+
+    Thank you!
+    We've added your card details
+    Continue
+    </>
   );
 }
 
